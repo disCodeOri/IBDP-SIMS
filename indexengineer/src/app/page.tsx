036776cfc249751a24bfcbf71396973468e6b75e
Parth <pandapixel.94@gmail.com>
2024-11-03 13:13:47 +0530

    Starting over the entire project as I am literally not able to modularise it in the slightest.

--- indexengineer/src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-// src/app/page.tsx
-"use client";
-
-import React from 'react';
-import SearchNavigation from '@/components/SearchNavigation';
-
-export default function Home() {
-  return <SearchNavigation />;
-}
