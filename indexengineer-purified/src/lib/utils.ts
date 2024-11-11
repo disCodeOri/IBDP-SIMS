8117a278d353313ce2e8397674b5ecb878af77b9
Parth Sankhla <pandapixel.94@gmail.com>
2024-11-11 17:10:20 +0530

    merged the modularised folder to the main folder of index engineer and deleted the purified version.

--- indexengineer-purified/src/lib/utils.ts
+++
@@ -1,8 +0,0 @@
-// This file is only for shadcn components (Parth on 1st nov 2024)
-
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
