d92d9cbd992a24e3bbeaf46665b6b9ffa495febf
parth9102007 <parth9102007@gmail.com>
2025-01-27 00:35:54 +0000

    removed some testing code for the multi space implementation.

--- kill-dem/src/app/components/WorkStage.tsx
+++ kill-dem/src/app/components/WorkStage.tsx
@@ -6,24 +6,6 @@ export default function WorkStage() {
 
   return (
     <div className="p-4 bg-gray-100">
-      <div className="mb-4 flex gap-2">
-        <button 
-          onClick={() => setCurrentSpace(p => Math.max(0, p - 1))}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          Previous Space
-        </button>
-        <button
-          onClick={() => setCurrentSpace(p => p + 1)}
-          className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-        >
-          Next Space
-        </button>
-        <div className="ml-4 p-2 self-center">
-          Current Space: {currentSpace + 1}
-        </div>
-      </div>
-
       <Manager
         size={[1280, 700]}
         style={{
