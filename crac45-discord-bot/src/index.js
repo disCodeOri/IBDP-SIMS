9a4939cb1c6c9ca9e24ad55a0fbdea65b53b916c
Parth Sankhla <pandapixel.94@gmail.com>
2025-03-14 11:35:34 +0530

    deleted unnecessary stuff due to laptop storage limitations and for cleanliness

--- crac45-discord-bot/src/index.js
+++
@@ -1,7 +0,0 @@
-// src/index.js
-const bot = require('./bot'); // Import the bot instance
-const { client } = bot; // Destructure the client object
-
-client.on('ready', () => {
-  console.log('Bot is ready!');
-})
